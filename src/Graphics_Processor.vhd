704ced1173371753a86274b5ecd17ebf16d5abc6
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2021-10-22 02:22:17 -0400

    instantiating ram to ensure compilaton works

--- src/Graphics_Processor.vhd
+++ src/Graphics_Processor.vhd
@@ -2,27 +2,33 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity Graphics_Processor is
-	-- port (
-	-- 	clk, rst : in std_logic;
+	port (
+		clk, rst : in std_logic;
 
-	-- 	vsync, hsync : out std_logic;
-	-- 	r, g, b : out std_logic_vector(3 downto 0)
-	-- );
+		vsync, hsync : out std_logic;
+		r, g, b : out std_logic_vector(3 downto 0)
+	);
 end Graphics_Processor;
 
 architecture default of Graphics_Processor is
 begin
 
-	-- U_VGA : entity work.vga
-	-- 	port map(
-	-- 		clk => clk,
-	-- 		rst => rst,
+	U_VGA : entity work.vga_top
+		port map(
+			clk => clk,
+			rst => rst,
 
-	-- 		vga_hsync => hsync,
-	-- 		vga_vsync => vsync,
-	-- 		r => r,
-	-- 		g => g,
-	-- 		b => b
-	-- 	);
+			vga_hsync => hsync,
+			vga_vsync => vsync,
+			r => r,
+			g => g,
+			b => b,
+
+			-- Unused (will likely be removed for this project)
+			cpu_says_swap_buf => '0',
+			wraddr => (others => '0'),
+			data => (others => '0'),
+			back_buf_wren => '0'
+		);
 
 end default;
