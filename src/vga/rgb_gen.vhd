17eb897fe90fbf97ac91c8fe32619bfdde35f55a
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2021-12-04 23:34:00 -0500

    uart controls note movement, added dyn array size

--- src/vga/rgb_gen.vhd
+++ src/vga/rgb_gen.vhd
@@ -21,6 +21,7 @@ port (
 	-- back_buf_wren : in std_logic;
 	
 	r, g, b : out std_logic_vector(3 downto 0);
+
 	uart_rx : in std_logic -- uart rx input line
 );
 end rgb_gen;
@@ -47,19 +48,23 @@ architecture bhv of rgb_gen is
 	signal staff_color : std_logic_vector(combined_color'range);
 	-- signal note_color: std_logic_vector(combined_color'range); -- 12 bits for R,G,B out
 
-	type natural_arr_t is array(0 to 4) of natural;
+	-- Unconstrained array, add array length later!
+	type natural_arr_t is array(natural range <>) of natural;
 	
-	constant new_y_vals : natural_arr_t := (NOTE_LOW_E_HEIGHT, NOTE_LOW_G_HEIGHT, NOTE_B_HEIGHT, NOTE_HIGH_D_HEIGHT, NOTE_HIGH_F_HEIGHT);
+	constant NUMBER_OF_NOTES : natural := 6;
+
+	constant new_y_vals : natural_arr_t(0 to NUMBER_OF_NOTES-1) := (NOTE_LOW_D_HEIGHT, NOTE_LOW_F_HEIGHT, NOTE_A_HEIGHT, NOTE_C_HEIGHT, NOTE_HIGH_E_HEIGHT, NOTE_HIGH_G_HEIGHT);
+
 
 	-- note: range for these is 0-15
-	constant color_arr_r : natural_arr_t := (15, 0, 0, 10, 15);
-	constant color_arr_g : natural_arr_t := (0, 15, 0, 10, 15);
-	constant color_arr_b : natural_arr_t := (0, 0, 15, 10, 15);
+	constant color_arr_r : natural_arr_t(0 to NUMBER_OF_NOTES-1) := (15, 0, 0, 15, 15, 0);
+	constant color_arr_g : natural_arr_t(0 to NUMBER_OF_NOTES-1) := (0, 15, 0, 15, 0, 15);
+	constant color_arr_b : natural_arr_t(0 to NUMBER_OF_NOTES-1) := (0, 0, 15, 0, 15, 15);
 
 
-	signal new_x_vals : natural_arr_t;
+	signal new_x_vals, curr_x_vals : natural_arr_t(0 to NUMBER_OF_NOTES-1);
 
-	type note_colors_t is array(0 to 4) of std_logic_vector(combined_color'range);
+	type note_colors_t is array(0 to NUMBER_OF_NOTES-1) of std_logic_vector(combined_color'range);
 
 	signal note_colors : note_colors_t;
 
@@ -68,13 +73,8 @@ architecture bhv of rgb_gen is
 	signal rx_byte, rx_byte_reg : std_logic_vector(7 downto 0);
 	signal reset_uart_buf : std_logic;
 
-	-- function any_or (a : note_colors_t) return std_logic_vector(11 downto 0) is
-	-- 	variable temp : std_logic_vector(11 downto 0) := (others => '0');
-	-- begin
-	-- 	for i in 0 to 4 loop
-	-- 		temp := temp or a(i);
-	-- 	end loop;
-	
+	signal advance_notes : std_logic;
+
 	component uart
 	generic(
 		baud_rate : natural := 9600;
@@ -82,10 +82,10 @@ architecture bhv of rgb_gen is
 	);
 	port (
 		clk, rst: in std_logic;
-
+		
 		rx : in std_logic; -- incoming serial line
 		tx : out std_logic; -- outgoing serial line
-
+		
 		transmit: in std_logic; -- transmit begins transmission
 		tx_byte : in std_logic_vector(7 downto 0); -- signal to transmit
 
@@ -102,7 +102,7 @@ architecture bhv of rgb_gen is
 	-- Reduce array of vectors into a single vector, using the OR operator.
 	function or_reduce(a : note_colors_t) return std_logic_vector is
 		variable ret : std_logic_vector(11 downto 0) := (others => '0');
-begin
+	begin
 		for i in a'range loop
 			ret := ret or a(i);
 		end loop;
@@ -129,7 +129,7 @@ begin
 		color => staff_color
 	);
 
-	U_NOTES: for i in 0 to 4 generate
+	U_NOTES: for i in 0 to NUMBER_OF_NOTES-1 generate
 		-- for testing
 		U_NOTE: entity work.rect
 		generic map(
@@ -149,39 +149,36 @@ begin
 			new_x => new_x_vals(i),
 			new_y => new_y_vals(i),
 
+			x => curr_x_vals(i),
+			y => open, -- we won't be changing the y based off it's current pos
+
 			color => note_colors(i)
 		);
 	end generate U_NOTES;
 
-	-- a simple counter to move the note around.
+	-- Controller for note movement, based on UART.
 	process(clk, rst)
 	begin
 		if(rst = '1')then
-			count <= 0;
-
-			for i in 0 to 4 loop
+			for i in 0 to NUMBER_OF_NOTES-1 loop
 				new_x_vals(i) <= 600; -- Start them off screen right
 			end loop;
 
 		elsif(rising_edge(clk)) then
-			count <= count + 1;
-
 			update_note <= '0';
 			
 			-- Default value for new x values
-			for i in 0 to 4 loop
+			for i in 0 to NUMBER_OF_NOTES-1 loop
 				new_x_vals(i) <= new_x_vals(i);
 			end loop;
 
-			if(count = 100000) then -- 100k
-
+			if(advance_notes = '1') then
 				-- Move each note to the left.
-				for i in 0 to 4 loop
+				for i in 0 to NUMBER_OF_NOTES-1 loop
 					new_x_vals(i) <= new_x_vals(i) - 1; -- latch inference OK
 				end loop;
 
 				update_note <= '1';
-				count <= 0;
 			end if;
 
 		end if;
@@ -208,4 +205,68 @@ begin
 		rx_samples => open,
 		rx_sample_countdown => open
 	);
+
+	-- Save incoming UART data (TODO into FIFO? hopefully not)
+	process(clk, rst)
+	begin
+		if(rst = '1') then
+			rx_byte_reg <= (others => '0');
+		elsif(rising_edge(clk)) then
+			if(received = '1') then
+				rx_byte_reg <= rx_byte;
+			elsif(reset_uart_buf = '1') then
+				rx_byte_reg <= (others => '0');
+			end if;
+		end if;
+	end process;
+
+
+	-- Combinational logic to create notes and update them.
+	process(rx_byte_reg)
+	begin
+		-- Default values
+		advance_notes <= '0';
+		reset_uart_buf <= '0';
+
+		case rx_byte_reg is
+			when x"1D" => -- todo make low D
+				null;
+			when x"1E" => -- 1st line "low" E
+				null;
+
+			when x"1F" => -- low F
+				null;
+
+			when x"20" => -- 2nd line G
+				null;
+
+			when x"1A" => -- low A
+				null;
+
+			when x"1B" => -- 3rd line B
+				null;
+
+			when x"1C" => -- C
+				null;
+
+			when x"2D" => -- 4th line "high" D
+				null;
+
+			when x"2E" => -- high E
+				null;
+
+			when x"2F" => -- high F
+				null;
+
+			when x"30" => -- high G
+				null;
+
+			when x"41" => -- char "A"
+				advance_notes <= '1';
+				reset_uart_buf <= '1';
+			
+			when others => null;
+		end case;
+	end process;
+
 end bhv;
