e42bf457576fadc59c811046fd16f7d73780f5ee
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2021-12-05 16:43:02 -0500

    rewrote with for generate & new rect api

--- src/vga/staff.vhd
+++ src/vga/staff.vhd
@@ -16,9 +16,80 @@ port(
 end staff;
 
 architecture default of staff is
-    signal vert_bar_color, g_ledger_color, d_ledger_color, b_ledger_color, f_ledger_color, e_ledger_color : std_logic_vector(color'range);
-begin
+    --Constants------------------------------------------------------
+    constant NUM_LINES : natural := 5; -- 5 ledgers
+    
 
+    --Type declarations-----------------------------------------------------
+    -- Array to hold color values from every note.
+    type color_array_t is array(0 to NUM_LINES-1) of std_logic_vector(color'range);
+    type param_array_t is array(0 to NUM_LINES-1) of natural;
+
+    --Function Definitions---------------------------------------------------
+    -- Reduce array of vectors into single vector using OR operator
+	function or_reduce(a : color_array_t) return std_logic_vector is
+		variable ret : std_logic_vector(11 downto 0) := (others => '0');
+	begin
+		for i in a'range loop
+			ret := ret or a(i);
+		end loop;
+
+		return ret;
+	end function or_reduce;
+
+
+    --Signals--------------------------------------------------------------------
+    signal line_colors : color_array_t;
+    signal vert_bar_color : std_logic_vector(color'range);
+    
+
+    --Line parameters-----------------------------------------------------------
+    constant LINE_HEIGHTS : param_array_t := (E_LINE_HEIGHT, G_LINE_HEIGHT, B_LINE_HEIGHT, D_LINE_HEIGHT, F_LINE_HEIGHT);
+
+
+begin --default--------------------------------------------------------------
+
+    -- Combine colors together for output.
+    color <= vert_bar_color or or_reduce(line_colors);
+
+    U_LEDGER_LINES: for i in 0 to NUM_LINES-1 generate
+        U_LEDGER_LINE: entity work.rect
+        generic map(
+            w => LEDGER_WIDTH,
+            h => LINE_THICKNESS,
+
+            x0 => LEDGER_X0, -- centerscreen minus offset (center of shape)
+            y0 => LINE_HEIGHTS(i),
+
+            r => 15,
+            g => 15,
+            b => 15
+        )
+        port map(
+            clk,
+            rst,
+            hcount,
+            vcount,
+            
+            -- Never move x or y.
+            update_x => '0',
+            reset_x => '0',
+    
+            set_y => '0',
+            new_y => 0,
+    
+            -- This should always be visible.
+            new_visible => '1',
+            set_visible => '1',
+    
+            curr_x => open,
+            curr_y => open,
+    
+            color => line_colors(i)
+        );
+    end generate U_LEDGER_LINES;
+
+    
     U_VERT_BAR: entity work.rect
     generic map(
         w => LINE_THICKNESS,
@@ -53,137 +124,5 @@ begin
 
         color => vert_bar_color
     );
-    
-
-    -- Draw ledger lines
-    U_F_LEDGER: entity work.rect
-    generic map(
-        w => LEDGER_WIDTH,
-        h => LINE_THICKNESS,
-
-        x0 => LEDGER_X0, -- centerscreen minus offset: center of shape
-        y0 => F_LINE_HEIGHT,
-
-        r => 15,
-        g => 15,
-        b => 15
-    )
-    port map(
-        clk,
-        rst,
-        hcount,
-        vcount,
-        update => '0',
-
-        new_x => 0, -- unused
-        new_y => 0, -- unused
-        
-        color => f_ledger_color
-    );
-
-    U_D_LEDGER: entity work.rect
-    generic map(
-        w => LEDGER_WIDTH,
-        h => LINE_THICKNESS,
-
-        x0 => LEDGER_X0, -- centerscreen minus offset: center of shape
-        y0 => D_LINE_HEIGHT,
-
-        r => 15,
-        g => 15,
-        b => 15
-    )
-    port map(
-        clk,
-        rst,
-        hcount,
-        vcount,
-        update => '0',
-
-        new_x => 0, -- unused
-        new_y => 0, -- unused
-        
-        color => d_ledger_color
-    );
-
-    U_B_LEDGER: entity work.rect
-    generic map(
-        w => LEDGER_WIDTH,
-        h => LINE_THICKNESS,
-
-        x0 => LEDGER_X0,
-        y0 => B_LINE_HEIGHT,
-
-        r => 15,
-        g => 15,
-        b => 15
-    )
-    port map(
-        clk,
-        rst,
-        hcount,
-        vcount,
-        update => '0',
-
-        new_x => 0, -- unused
-        new_y => 0, -- unused
-        
-        color => b_ledger_color
-    );
-
-    U_G_LEDGER: entity work.rect
-    generic map(
-        w => LEDGER_WIDTH,
-        h => LINE_THICKNESS,
-
-        x0 => LEDGER_X0, -- centerscreen minus offset: center of shape
-        y0 => G_LINE_HEIGHT,
-
-        r => 15,
-        g => 15,
-        b => 15
-    )
-    port map(
-        clk,
-        rst,
-        hcount,
-        vcount,
-        update => '0',
-
-        new_x => 0, -- unused
-        new_y => 0, -- unused
-        
-        color => g_ledger_color
-    );   
-
-    U_E_LEDGER: entity work.rect
-    generic map(
-        w => LEDGER_WIDTH,
-        h => LINE_THICKNESS,
-
-        x0 => LEDGER_X0, -- centerscreen minus offset: center of shape
-        y0 => E_LINE_HEIGHT,
-
-        r => 15,
-        g => 15,
-        b => 15
-    )
-    port map(
-        clk,
-        rst,
-        hcount,
-        vcount,
-        update => '0',
-
-        new_x => 0, -- unused
-        new_y => 0, -- unused
-        
-        color => e_ledger_color
-    );   
-
-
-
-
-    color <= vert_bar_color or f_ledger_color or d_ledger_color or b_ledger_color or g_ledger_color or e_ledger_color;
 
 end default;
