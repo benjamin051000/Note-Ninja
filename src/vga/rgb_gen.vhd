e2f3f5ed6da5a8de1e82c79c843cfa254147c1c2
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2021-12-05 13:34:41 -0500

    [wip] bugged, todo refactor rgbgen

--- src/vga/rgb_gen.vhd
+++ src/vga/rgb_gen.vhd
@@ -53,7 +53,7 @@ architecture bhv of rgb_gen is
 	
 	constant NUMBER_OF_NOTES : natural := 6;
 
-	constant new_y_vals : natural_arr_t(0 to NUMBER_OF_NOTES-1) := (NOTE_LOW_D_HEIGHT, NOTE_LOW_F_HEIGHT, NOTE_A_HEIGHT, NOTE_C_HEIGHT, NOTE_HIGH_E_HEIGHT, NOTE_HIGH_G_HEIGHT);
+	-- constant new_y_vals : natural_arr_t(0 to NUMBER_OF_NOTES-1) := (NOTE_LOW_D_HEIGHT, NOTE_LOW_F_HEIGHT, NOTE_A_HEIGHT, NOTE_C_HEIGHT, NOTE_HIGH_E_HEIGHT, NOTE_HIGH_G_HEIGHT);
 
 
 	-- note: range for these is 0-15
@@ -62,14 +62,17 @@ architecture bhv of rgb_gen is
 	constant color_arr_b : natural_arr_t(0 to NUMBER_OF_NOTES-1) := (0, 0, 15, 0, 15, 15);
 
 
-	signal new_x_vals, curr_x_vals : natural_arr_t(0 to NUMBER_OF_NOTES-1);
+	signal new_x_vals, curr_x_vals, new_y_vals : natural_arr_t(0 to NUMBER_OF_NOTES-1);
 
 	type note_colors_t is array(0 to NUMBER_OF_NOTES-1) of std_logic_vector(combined_color'range);
-
 	signal note_colors : note_colors_t;
 
+	-- type visible_arr is array(0 to NUMBER_OF_NOTES-1) of std_logic;
+	signal visibles : std_logic_vector(NUMBER_OF_NOTES-1 downto 0);
+	signal notes_pos_reset_arr : std_logic_vector(NUMBER_OF_NOTES-1 downto 0);
+
 	----------------------------------------------------------
-	signal received, is_receiving : std_logic;
+	signal received : std_logic;
 	signal rx_byte, rx_byte_reg : std_logic_vector(7 downto 0);
 	signal reset_uart_buf : std_logic;
 
@@ -110,9 +113,8 @@ architecture bhv of rgb_gen is
 		return ret;
 	end function or_reduce;
 
-begin
+begin -- bhv
 	combined_color <= staff_color or or_reduce(note_colors);
-
 
 	r <= combined_color(11 downto 8);
 	g <= combined_color(7 downto 4);
@@ -149,6 +151,8 @@ begin
 			new_x => new_x_vals(i),
 			new_y => new_y_vals(i),
 
+			visible => visibles(i),
+
 			x => curr_x_vals(i),
 			y => open, -- we won't be changing the y based off it's current pos
 
@@ -161,7 +165,7 @@ begin
 	begin
 		if(rst = '1')then
 			for i in 0 to NUMBER_OF_NOTES-1 loop
-				new_x_vals(i) <= 600; -- Start them off screen right
+				new_x_vals(i) <= 640; -- Start them off screen right
 			end loop;
 
 		elsif(rising_edge(clk)) then
@@ -169,7 +173,12 @@ begin
 			
 			-- Default value for new x values
 			for i in 0 to NUMBER_OF_NOTES-1 loop
-				new_x_vals(i) <= new_x_vals(i);
+				if(notes_pos_reset_arr(i) = '1') then
+					new_x_vals(i) <= 640; -- Reset this particular note.
+
+				else
+					new_x_vals(i) <= new_x_vals(i);
+				end if;
 			end loop;
 
 			if(advance_notes = '1') then
@@ -199,7 +208,7 @@ begin
 		tx_byte => (others => '0'),
 		received => received, -- will be asserted for one clock cycle.
 		rx_byte => rx_byte,
-		is_receiving => is_receiving,
+		is_receiving => open,
 		is_transmitting => open,
 		recv_error => open,
 		rx_samples => open,
@@ -221,52 +230,100 @@ begin
 	end process;
 
 
-	-- Combinational logic to create notes and update them.
-	process(rx_byte_reg)
+	-- Logic to create notes and update them.
+	process(clk, rst)
+		variable note_to_create_idx : natural;
+		variable pitch : natural;
+		variable create_new_note : std_logic;
 	begin
-		-- Default values
-		advance_notes <= '0';
-		reset_uart_buf <= '0';
+		if(rst = '1') then
+			-- Default values
+			advance_notes <= '0';
+			reset_uart_buf <= '0';
 
-		case rx_byte_reg is
-			when x"1D" => -- todo make low D
-				null;
-			when x"1E" => -- 1st line "low" E
-				null;
-
-			when x"1F" => -- low F
-				null;
-
-			when x"20" => -- 2nd line G
-				null;
-
-			when x"1A" => -- low A
-				null;
-
-			when x"1B" => -- 3rd line B
-				null;
-
-			when x"1C" => -- C
-				null;
-
-			when x"2D" => -- 4th line "high" D
-				null;
-
-			when x"2E" => -- high E
-				null;
-
-			when x"2F" => -- high F
-				null;
-
-			when x"30" => -- high G
-				null;
-
-			when x"41" => -- char "A"
-				advance_notes <= '1';
-				reset_uart_buf <= '1';
+			create_new_note := '0';
+			note_to_create_idx := 0;
+			pitch := 0;
+			notes_pos_reset_arr <= (others => '0');
+			visibles <= (others => '0');
 			
-			when others => null;
-		end case;
+		elsif(rising_edge(clk)) then
+			-- Default values
+			advance_notes <= '0';
+			reset_uart_buf <= '0';
+
+			create_new_note := '0';
+			note_to_create_idx := 0;
+			pitch := 0;
+			notes_pos_reset_arr <= (others => '0');
+
+			case rx_byte_reg is
+				when x"41" => -- char "A"
+					advance_notes <= '1';
+					reset_uart_buf <= '1';
+
+				when x"1D" => -- low D
+					create_new_note := '1';
+					pitch := NOTE_LOW_D_HEIGHT;
+
+				when x"1E" => -- 1st line "low" E
+					create_new_note := '1';
+					pitch := NOTE_LOW_E_HEIGHT;
+
+				when x"1F" => -- low F
+					create_new_note := '1';
+					pitch := NOTE_LOW_F_HEIGHT;
+
+				when x"20" => -- 2nd line "low" G
+					create_new_note := '1';
+					pitch := NOTE_LOW_G_HEIGHT;
+
+				when x"1A" => -- 2nd space A
+					create_new_note := '1';
+					pitch := NOTE_A_HEIGHT;
+
+				when x"1B" => -- 3rd line B
+					create_new_note := '1';
+					pitch := NOTE_B_HEIGHT;
+
+				when x"1C" => -- C
+					create_new_note := '1';
+					pitch := NOTE_C_HEIGHT;
+
+				when x"2D" => -- 4th line "high" D
+					create_new_note := '1';
+					pitch := NOTE_HIGH_D_HEIGHT;
+
+				when x"2E" => -- high E
+					create_new_note := '1';
+					pitch := NOTE_HIGH_E_HEIGHT;
+
+				when x"2F" => -- high F
+					create_new_note := '1';
+					pitch := NOTE_HIGH_F_HEIGHT;
+
+				when x"30" => -- high G
+					create_new_note := '1';
+					pitch := NOTE_HIGH_G_HEIGHT;
+				
+				when others => null;
+			end case;
+
+			if(create_new_note = '1') then
+				-- 1) Pick note that is currently invisible
+				for i in 0 to NUMBER_OF_NOTES-1 loop
+					if(visibles(i) = '0') then
+						note_to_create_idx := i;
+						exit; -- TODO remove if buggy, it's not completely necessary
+					end if;
+				end loop;
+
+				-- 2) Use note_to_create_idx index to create a new note.
+				new_y_vals(note_to_create_idx) <= pitch;
+				notes_pos_reset_arr(note_to_create_idx) <= '1'; -- Start it off-screen
+				visibles(note_to_create_idx) <= '1'; -- Note is now visible.
+			end if;
+		end if;
 	end process;
 
 end bhv;
